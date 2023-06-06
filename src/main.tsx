155f8322290f59afb148b7f7859a03427aacf890
Alexandr Opryshko <sclif13@gmail.com>
2023-06-06 17:26:03 +0500

    prettier & eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { ThemeProvider } from 'styled-components';
+import { LIGHT_THEME, DropdownProvider, FontsVTBGroup } from '@admiral-ds/react-ui';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={LIGHT_THEME}>
+      <DropdownProvider>
+        <FontsVTBGroup />
+        <App />
+      </DropdownProvider>
+    </ThemeProvider>
   </React.StrictMode>,
-)
+);
