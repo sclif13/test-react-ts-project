40722de4eb36c29eb4d191ca091057cdb893ceb9
Alexandr Opryshko <sclif13@gmail.com>
2023-06-07 17:44:58 +0500

    rm shared

--- shared/src/components/styled/StyledT.tsx
+++
@@ -1,7 +0,0 @@
-import { T } from '@admiral-ds/react-ui'
-import styled from 'styled-components'
-import { spacer, SpacingAttr } from '../../helpers/spacer'
-
-export const StyledT = styled(T) <SpacingAttr>`
-  ${spacer};
-`;
